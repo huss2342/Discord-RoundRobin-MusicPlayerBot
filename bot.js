d9c802971104dab5dd1f9f987b0a5b17532d560f
huss2342 <89368091+huss2342@users.noreply.github.com>
2023-12-12 11:23:10 -0600

    nice and clean

--- bot.js
+++ bot.js
@@ -1,110 +1,177 @@
-const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
-const { token, clientId, guildId } = require('./config.json');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType, AudioPlayerStatus } = require('@discordjs/voice');
-const ytdl = require('ytdl-core');
-const fs = require('fs');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+const { Client, GatewayIntentBits, REST, Routes } = require("discord.js");
+const { token, clientId, guildId } = require("./config.json");
+const {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  StreamType,
+  AudioPlayerStatus,
+} = require("@discordjs/voice");
+const ytdl = require("ytdl-core");
+const fs = require("fs");
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+});
+const { cleanupSongs, createTempFileForGuild } = require("./utils");
 
-client.once('ready', () => {
-  console.log('------------------------------');
-  console.log('Ready!');
+client.once("ready", () => {
+  console.log("------------------------------");
+  console.log("Ready!");
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-  
-    const { commandName } = interaction;
-  
-    switch (commandName) {
-        case 'ping':
-            pong(interaction);
-            break;
+// --------------- FSM TO HANDLE COMMANDS ----------------
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-        case 'say':
-            say(interaction);
-            break;
-        
-        case 'play':
-            play(interaction);
-            break;
-    }
+  const { commandName } = interaction;
+
+  switch (commandName) {
+    case "ping":
+      pong(interaction);
+      break;
+
+    case "say":
+      say(interaction);
+      break;
+
+    case "play":
+      play(interaction);
+      break;
+  }
+});
+
+// ------------- Global Structures ----------------
+let queue = [];
+let isPlaying = false;
+let isDisconnected = false;
+let hasHandledDisconnect = false;
+// --------------- COMMANDS ----------------
+
+// --------------- Playing a song ----------------
+async function play(interaction) {
+  const voiceChannel = interaction.member.voice.channel;
+  if (!voiceChannel) {
+    await interaction.reply("You need to be in a voice channel!");
+    return;
+  }
+
+  const songUrl = interaction.options.getString("url");
+  if (!songUrl || !ytdl.validateURL(songUrl)) {
+    await interaction.reply("Please provide a valid YouTube URL.");
+    return;
+  }
+
+  // Add to queue
+  const songPath = createTempFileForGuild(interaction.guildId);
+  queue.push({
+    url: songUrl,
+    path: songPath,
   });
-  
 
+  // Download the song and then play if necessary
+  await downloadSong(songUrl, songPath);
 
-  async function play(interaction) {
-    // Check if the user is in a voice channel
-    const voiceChannel = interaction.member.voice.channel;
-    if (!voiceChannel) {
-        await interaction.reply('You need to be in a voice channel!');
-        return;
+  if (!isPlaying) {
+    console.log("playing next song");
+    await playNextSong(voiceChannel, interaction.channel); // Pass only voiceChannel
+    await interaction.reply(`Starting...`);
+  } else {
+    await interaction.reply(`Song added to the queue.`);
+  }
+}
+
+async function downloadSong(url, path) {
+  const stream = ytdl(url, { filter: "audioonly" });
+  const writer = fs.createWriteStream(path);
+  stream.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on("finish", resolve);
+    writer.on("error", reject);
+  });
+}
+
+async function playNextSong(voiceChannel, textChannel) {
+  if (queue.length === 0) {
+    isPlaying = false;
+    return;
+  }
+
+  isPlaying = true;
+  const song = queue.shift();
+
+  try {
+    const resource = createAudioResource(song.path, {
+      inputType: StreamType.Arbitrary,
+    });
+
+    const player = createAudioPlayer();
+    player.play(resource);
+
+    const connection = joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: voiceChannel.guild.id,
+      adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+    });
+    connection.subscribe(player);
+
+    // Send a message to the channel about the song being played
+    console.log(`textChannel: ${textChannel}`);
+    console.log(`textChannel.type: ${textChannel.type}`);
+    if (textChannel && textChannel.type === 0) {
+      textChannel.send(`Now playing: ${song.url}`);
     }
+    console.log(`Now playing: ${song.url}`);
 
-    // Get the song URL from the interaction
-    const songUrl = interaction.options.getString('url');
-    if (!songUrl || !ytdl.validateURL(songUrl)) {
-        await interaction.reply('Please provide a valid YouTube URL.');
-        return;
-    }
-
-    // Define a temporary file path for the song
-    const songPath = `./temp_song_${Date.now()}.mp3`;
-
-    try {
-        // Download the song from YouTube
-        const stream = ytdl(songUrl, { filter: 'audioonly' });
-        const writer = fs.createWriteStream(songPath);
-        stream.pipe(writer);
-
-        await new Promise((resolve, reject) => {
-            writer.on('finish', resolve);
-            writer.on('error', reject);
-        });
-
-        // Create an audio resource from the downloaded file
-        const resource = createAudioResource(songPath, { inputType: StreamType.Arbitrary });
-
-        // Create an audio player and play the resource
-        const player = createAudioPlayer();
-        player.play(resource);
-
-        // Join the voice channel and subscribe the player to the connection
-        const connection = joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: interaction.guildId,
-            adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-        });
-        connection.subscribe(player);
-
-        // Reply to the user
-        await interaction.reply(`Now playing: ${songUrl}`);
-
-        // Listen for the audio player to become idle and then clean up
-        player.on(AudioPlayerStatus.Idle, () => {
-            connection.destroy();
-            fs.unlink(songPath, (err) => {
-                if (err) console.error('Error deleting song file:', err);
-            });
-        });
-    } catch (error) {
-        console.error('Error playing song:', error);
-        if (fs.existsSync(songPath)) {
-            fs.unlink(songPath, (err) => {
-                if (err) console.error('Error deleting song file:', err);
-            });
+    // When connection state changes
+    connection.on('stateChange', (oldState, newState) => {
+        if (newState.status === 'disconnected' && !hasHandledDisconnect) {
+            console.log('Bot has been disconnected from the voice channel.');
+            hasHandledDisconnect = true;
+    
+            if (player) {
+                player.stop();
+                player.removeAllListeners(AudioPlayerStatus.Idle);
+            }
+    
+            cleanupSongs(voiceChannel.guild.id);
+            isPlaying = false;
+            isDisconnected = true; // Set the disconnection flag
         }
-        await interaction.reply('There was an error trying to play that song!');
+    });
+
+    // When the player becomes idle
+    player.on(AudioPlayerStatus.Idle, () => {
+      if (isDisconnected) {
+        // If disconnected, reset the flag and don't proceed further
+        isDisconnected = false;
+        return;
+      }
+
+      fs.unlink(song.path, (err) => {
+        if (err) console.error("Error deleting song file:", err);
+      });
+      playNextSong(voiceChannel, textChannel);
+    });
+  } catch (error) {
+    console.error("Error playing song:", error);
+    if (fs.existsSync(song.path)) {
+      fs.unlink(song.path, (err) => {
+        if (err) console.error("Error deleting song file:", err);
+      });
     }
+  }
 }
 
-
+// --------------- Pong! ----------------
 async function pong(interaction) {
-    await interaction.reply('Pong!');
+  await interaction.reply("Pong!");
 }
 
+// --------------- Say something ----------------
 async function say(interaction) {
-    const message = interaction.options.getString('message');
-    await interaction.reply(message);
+  const message = interaction.options.getString("message");
+  await interaction.reply(message);
 }
 
 client.login(token);
---
+++ utils.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const fs = require('fs');
+
+
+function cleanupSongs(guildId) {
+    console.log(`!!!!!!Cleaning up songs for guild ID ${guildId}`);
+    const guildDirectory = path.join(__dirname, `temp_${guildId}`);
+
+    if (fs.existsSync(guildDirectory)) {
+        fs.readdir(guildDirectory, (err, files) => {
+            if (err) {
+                console.error(`Error reading directory for cleanup: ${guildDirectory}`, err);
+                return;
+            }
+
+            files.forEach(file => {
+                const filePath = path.join(guildDirectory, file);
+                fs.unlink(filePath, err => {
+                    if (err) {
+                        console.error(`Error deleting file ${filePath}:`, err);
+                    } else {
+                        console.log(`Deleted song file: ${filePath}`);
+                    }
+                });
+            });
+
+            // remove the directory as well
+            fs.rm(guildDirectory, { recursive: true }, err => {
+                if (err) {
+                    console.error(`Error deleting directory ${guildDirectory}:`, err);
+                } else {
+                    console.log(`Deleted directory: ${guildDirectory}`);
+                }
+            });
+        });
+    } else {
+        console.log(`No directory found for guild ID ${guildId}, no cleanup necessary.`);
+    }
+}
+
+
+function createTempFileForGuild(guildId) {
+    const guildDirectory = path.join(__dirname, `temp_${guildId}`);
+    if (!fs.existsSync(guildDirectory)) {
+        fs.mkdirSync(guildDirectory, { recursive: true });
+    }
+
+    // Create a unique file name
+    const fileName = `song_${Date.now()}.mp3`;
+    const filePath = path.join(guildDirectory, fileName);
+
+    return filePath;
+}
+
+module.exports = {
+    cleanupSongs,
+    createTempFileForGuild,
+}
